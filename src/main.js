815b1ff7e126952eb133eddfe1cc122a1c25dc6a
Courtney Solari <csolari@ufl.edu>
2020-07-27 13:02:25 -0500

    Fix mobile issues and update mobile menu behavior

--- src/main.js
+++ src/main.js
@@ -2,18 +2,29 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	const menuClose 	= 	document.getElementById('close-menu');
 	const menuOpen 		= 	document.getElementById('open-mobile-menu');
 	const mobileMenu 	= 	document.getElementsByClassName('mobile-menu')[0];
+	const mobileLinks 	=	document.getElementsByClassName('mobile-link');
 
-	// When the user clickes the mobile menu open button, display the mobile menu and hide the menu open button
-	menuOpen.addEventListener("click", function() {
+	// Open Menu Function
+	function openMenu() {
 		mobileMenu.style.top = "0";		// Reset right to 0 from where we accounted for added padding when hiding menu
 		mobileMenu.style.height = "100%";
 		menuOpen.classList.add("hide");
-	});
+	}
 
-	// When the user clicks the mobile menu close button, hide the mobile menu and display the menu open button
-	menuClose.addEventListener("click", function () {
+	// Close Menu Function
+	function closeMenu() {
 		mobileMenu.style.height = "0";
 		mobileMenu.style.top = "-20%";
 		menuOpen.classList.remove("hide");
+	}
+
+	// When the user clickes the mobile menu open button, display the mobile menu and hide the menu open button
+	menuOpen.addEventListener("click", openMenu);
+
+	// When the user clicks the mobile menu close button, hide the mobile menu and display the menu open button
+	menuClose.addEventListener("click", closeMenu);
+
+	mobileLinks.forEach(link => {
+		link.addEventListener('click', closeMenu);
 	});
 });
